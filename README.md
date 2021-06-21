8d2adcd3570d9bc4bbaeedde0f8d73758680ec6c
Jake Klassen <jklassendev@gmail.com>
2021-06-21 09:26:20 -0400

    saving some early ECS progress

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 Breakout in JS/Canvas
 
 [Demo](https://breakout-js.netlify.app/) 🎮
+
+## ECS Definition
+
+| EntityId | Position | Color  | Component N ... |
+| -------- | -------- | ------ | --------------- |
+| 1        | {x, y}   | 'red'  |
+| 2        | {x, y}   | 'blue' |
+
+- Database table analogy
+- Entity = number
+- Need a way to create an entity
+- Need something to hold components
+- Need some way to get an entities component
+- Need something to hold all our entities and their components
+- Need something to run all our "game logic" (Systems)
+  - Systems are functions that operate on one or more entities with components of interest
+
+System: A way to query for components and update them
+
+## High Level Objective
+
+Move a colored square around the screen with collisions, using an ECS.
---
+++ src/ecs-demo.ts
@@ -0,0 +1,47 @@
+import renderWorldItems from "./ecs/renderingSystem";
+import World, { Color, Dimensions, Entity, Position, Shape } from "./ecs/world";
+
+const canvas = document.querySelector<HTMLCanvasElement>("canvas")!;
+const ctx = canvas.getContext("2d")!;
+
+let dt = 0;
+let last = performance.now();
+
+/**
+ * The game loop.
+ */
+function frame(hrt: DOMHighResTimeStamp) {
+  // How much time has elapsed since the last frame?
+  // Also convert to seconds.
+  dt = (hrt - last) / 1000;
+
+  // we need to work with our systems
+
+  last = hrt;
+
+  renderWorldItems(world, ctx);
+  // Keep the game loop going forever
+  requestAnimationFrame(frame);
+}
+
+// create the world
+const world = new World();
+const redSquare = new Entity();
+redSquare.addComponent(new Position());
+redSquare.addComponent(new Dimensions());
+redSquare.addComponent(new Color());
+redSquare.addComponent(new Shape());
+
+world.addWorldItem({
+  id: 1,
+  position: { x: 0, y: 0 },
+  dimensions: { width: 100, height: 100 },
+  color: "red",
+  shape: "rect",
+});
+
+// register our entities and their components
+// red square, with a position and velocity
+
+// we need to start the game
+reestAnimationFrame(frame);
