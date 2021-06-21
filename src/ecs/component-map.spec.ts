6219aa0e16ef6b19e97cb9b334befa6c94bfb8ce
Jake Klassen <jklassendev@gmail.com>
2021-06-21 19:30:45 -0400

    saving work on component map and world

---
+++ src/ecs/component-map.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentMap } from "./component-map";
+import { Component } from "./world";
+
+class Color extends Component {}
+class Rectangle extends Component {}
+
+describe("ComponentMap", () => {
+  it("should support adding components", () => {
+    const cm = new ComponentMap();
+
+    cm.add(new Color(), new Rectangle());
+
+    expect(cm.size).toEqual(2);
+    expect(cm.has(Color, Rectangle)).toEqual(true);
+  });
+
+  it("should support removing components", () => {
+    const cm = new ComponentMap();
+
+    cm.add(new Color(), new Rectangle());
+    cm.delete(Rectangle);
+
+    expect(cm.size).toEqual(1);
+    expect(cm.has(Rectangle)).toEqual(false);
+  });
+
+  it("should support clearing components", () => {
+    const cm = new ComponentMap();
+
+    cm.add(new Color(), new Rectangle());
+    cm.clear();
+
+    expect(cm.size).toEqual(0);
+    expect(cm.has(Color, Rectangle)).toEqual(false);
+  });
+});
---
+++ src/ecs/component-map.ts
@@ -0,0 +1,31 @@
+import { ComponentConstructor, Component } from "./world";
+
+export class ComponentMap {
+  #map = new Map<ComponentConstructor, Component>();
+
+  public add(...components: Component[]): void {
+    for (const component of components) {
+      this.#map.set(component.constructor as ComponentConstructor, component);
+    }
+  }
+
+  public delete(...componentConstructors: ComponentConstructor[]): void {
+    componentConstructors.forEach((constructor) => {
+      return this.#map.delete(constructor);
+    });
+  }
+
+  public clear() {
+    this.#map.clear();
+  }
+
+  get size(): number {
+    return this.#map.size;
+  }
+
+  public has(...componentConstructors: ComponentConstructor[]): boolean {
+    return componentConstructors.every((constructor) => {
+      return this.#map.has(constructor);
+    });
+  }
+}
