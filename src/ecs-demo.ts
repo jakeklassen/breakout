e985da366cdc0091ab9754377df1116b780ea67c
Jake Klassen <jklassendev@gmail.com>
2021-11-19 10:34:47 -0500

    remove play code

--- src/ecs-demo.ts
+++ src/ecs-demo.ts
@@ -101,8 +101,6 @@ class RenderingSystem extends System {
       const { width, height } = componentMap.get(Rectangle);
       const { x, y } = componentMap.get(Position);
 
-      componentMap.get(Position).x;
-
       this.context.fillStyle = color;
       this.context.fillRect(x, y, width, height);
     }
